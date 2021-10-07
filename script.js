974c5d8f2d8d7107b30664bf6e6c1b2df1bf810c
Moataz Sanad <msanad@ezzsteel.com.eg>
2021-10-07 07:06:36 +0200

    finalize portfolio

--- script.js
+++ script.js
@@ -12,4 +12,40 @@ $(document).ready(function () {
         $('.navbar .app-menu').toggleClass("show")
         $('.menu-btn i').toggleClass("show")
     })
+
+    var typed = new Typed(".typing", {
+        strings: ["Full Stack Developer", "Freelancer", "Data Scientist", "Product Manager"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing1", {
+        strings: ["Full Stack Developer", "Freelancer", "Data Scientist", "Product Manager"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
 })
